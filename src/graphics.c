769c351cfd268534c9c1d84943bcbb060a8fe021
Slashr2020 <dade55@rambler.ru>
2026-08-03 13:23:28 +0300

    Configure CMake and add rasterization

--- src/graphics.c
+++ src/graphics.c
@@ -7,22 +7,30 @@
 #include "vector_funcs.h"
 #include "matrix.h"
 
-#if 0
 float clamp(float value, float max, float min){
   return fmaxf(fminf(value,max),min);
 }
-#endif
 
-struct Edge {
-  int start; int end;
+struct vec3 baricentric_coords(vec3 v0, vec3 v1, vec3 v2, float x, float y){
+  float det = (v0.x - v2.x)*(v1.y - v2.y) - (v1.x - v2.x)*(v0.y - v2.y);
+
+  float l0 = ((v1.y - v2.y)*(x-v2.x) + (v2.x-v1.x)*(y-v2.y))/det; 
+  float l1 = ((v2.y - v0.y)*(x-v2.x) + (v0.x-v2.x)*(y-v2.y))/det; 
+  float l2 = 1.0f - l0 - l1; 
+  struct vec3 lambda = create_vec3(l0, l1, l2);
+  return lambda;
+}
+
+struct Triangle {
+  int v0,v1,v2;
 };
 
 struct Model3D {
   struct vec3* vertices;
   int vertices_count;
 
-  struct Edge* edges;
-  int edges_count;
+  struct Triangle* triangles;
+  int triangles_count;
 
   struct vec3 position;
   struct vec3 rotation;
@@ -48,9 +56,30 @@ void draw(int x, int y, float z, int width, int height, char* screen, float* z_b
       screen[y * (width+1) + x]=sym;
     }
   }
-
 }
 
+void bounding_box(vec3 v0, vec3 v1, vec3 v2, int width, int height, char* screen, float* z_buffer){
+  float min_x = fminf(v0.x, fminf(v1.x, v2.x));
+  float max_x = fmaxf(v0.x, fmaxf(v1.x, v2.x));
+
+  float min_y = fminf(v0.y, fminf(v1.y, v2.y));
+  float max_y = fmaxf(v0.y, fmaxf(v1.y, v2.y));
+
+  if (max_x >= width) max_x = width-1; if (min_x<0) min_x=0;
+  if (max_y >= height) max_y = height-1; if (min_y<0) min_y=0;
+
+  for (int y=min_y; y<=max_y; y++){
+    for (int x=min_x; x<=max_x; x++){
+      struct vec3 lambda = baricentric_coords(v0,v1,v2,(float)x,(float)y);
+      if (lambda.x>=0.0f && lambda.y>=0.0f && lambda.z>=0.0f){
+        float z = v0.z*lambda.x + v1.z*lambda.y + v2.z*lambda.z;
+        draw(x,y,z,width,height,screen,z_buffer,'@');
+      }
+    }
+  }
+
+}
+#if 0
 static inline void line(struct vec3 v0, struct vec3 v1,char* screen, float* z_buffer,int width,int height){
   int x0 = (int)v0.x; int x1 = (int)v1.x;
   int y0 = (int)v0.y; int y1 = (int)v1.y;
@@ -68,7 +97,6 @@ static inline void line(struct vec3 v0, struct vec3 v1,char* screen, float* z_bu
 
     draw(x0, y0, z0, width, height, screen, z_buffer, '@');
     if (x0==x1 && y0==y1) break;
-
     int e2 = 2*error;
     if (e2 > -dy){
       error -= dy;
@@ -81,6 +109,7 @@ static inline void line(struct vec3 v0, struct vec3 v1,char* screen, float* z_bu
     step++;
   }
 }
+#endif
 
 void draw_model(struct Model3D model, int width, int height, char* screen, float* z_buffer){
   struct mat4 transform_mat = get_model_mat(model);
@@ -92,11 +121,18 @@ void draw_model(struct Model3D model, int width, int height, char* screen, float
 
     pixel_vertices[i] = create_vec3(scr.x, scr.y, vertex.z);
   }
-  for (int i=0; i<model.edges_count; i++){
-    struct vec3 v0 = pixel_vertices[model.edges[i].start];
-    struct vec3 v1 = pixel_vertices[model.edges[i].end];
+  for (int i=0; i<model.triangles_count; i++){
+    struct Triangle t = model.triangles[i];
+    struct vec3 v0 = pixel_vertices[t.v0];
+    struct vec3 v1 = pixel_vertices[t.v1];
+    struct vec3 v2 = pixel_vertices[t.v2];
 
+    bounding_box(v0,v1,v2,width,height,screen,z_buffer);
+#if 0
     line(v0, v1, screen, z_buffer, width, height);    
+    line(v2, v1, screen, z_buffer, width, height);    
+    line(v2, v0, screen, z_buffer, width, height);    
+#endif
   }
 }
 
@@ -126,11 +162,13 @@ struct vec3 CUBE_VERTICES[8] = {
 
 };
 
-struct Edge CUBE_EDGES[12] = {
-  {0,1}, {1,2}, {2,3}, {3,0},
-  {4,5}, {5,6}, {6,7}, {7,4},
-  {0,4}, {1,5}, {2,6}, {3,7}
-
+struct Triangle CUBE_TRIANGLES[12] ={
+  {0,4,5}, {0,5,1}, // down
+  {3,0,1}, {3,1,2}, // back
+  {4,7,6}, {4,6,5}, // front
+  {7,3,2}, {7,2,6}, // up
+  {3,7,4}, {3,4,0}, // left
+  {1,5,6}, {1,6,2}, // right
 };
 
 int buffer_size = (WIDTH+1)*HEIGHT+1;
@@ -141,8 +179,8 @@ int main(){
   struct Model3D cube1;
   cube1.vertices = CUBE_VERTICES;
   cube1.vertices_count = 8;
-  cube1.edges = CUBE_EDGES;
-  cube1.edges_count = 12;
+  cube1.triangles = CUBE_TRIANGLES;
+  cube1.triangles_count = 12;
   cube1.position = (struct vec3){0.0f, 0.0f, 0.3f};
   cube1.rotation = (struct vec3){0,0,0};
   float angle = 0.0f;
@@ -152,8 +190,7 @@ int main(){
     
     angle+=0.03f;
     cube1.rotation = (struct vec3){0.0f, angle, 0.0f};
-
-
+    
     draw_model(cube1, WIDTH, HEIGHT, screen, z_buffer);
     printf("\x1b[H%s",screen);
     usleep(30000);
